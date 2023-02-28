e209a5df46cf7d2f302e2eb5cbd75a343a1fb97e
weejinyoung <a8118199@gmail.com>
2023-02-28 21:59:05 +0900

    [ feat ] 스프링 컨테이너 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,8 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
@@ -18,11 +20,13 @@ import static org.springframework.http.MediaType.*;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh();
+
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-			HelloController helloController = new HelloController();
-
 			servletContext.addServlet("FrontController", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -30,10 +34,10 @@ public class HellobootApplication {
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
+						HelloController helloController = applicationContext.getBean(HelloController.class);
 						String ret = helloController.hello(name);
 
-						resp.setStatus(200);
-						resp.setHeader(CONTENT_TYPE, TEXT_PLAIN_VALUE);
+						resp.setContentType(TEXT_PLAIN_VALUE);
 						resp.getWriter().println(ret);
 					} else if (req.getRequestURI().equals("/user")) {
 

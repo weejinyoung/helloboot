6660bc2a83039b5a8e910e7a0bc58f3ea1be8060
weejinyoung <a8118199@gmail.com>
2023-03-03 17:41:55 +0900

    [ feat ] DispatcherServlet 으로 전환

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,32 +22,14 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("FrontController", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 인증, 보안, 다국어, 공통 기능
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-
-						resp.setContentType(TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					} else if (req.getRequestURI().equals("/user")) {
-
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-
-					}
-				}
+			servletContext.addServlet("DispatcherServlet", new DispatcherServlet(applicationContext) {
 			}).addMapping("/*");
 		});
 		webServer.start();

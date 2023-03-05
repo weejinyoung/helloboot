066ba2fc2da218203600079838309dfc0fc5945b
weejinyoung <a8118199@gmail.com>
2023-03-05 23:34:08 +0900

    [ feat ] @Component 스캔

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,13 +1,17 @@
 package tobyspring.helloboot;
 
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RequestMapping("/hello")
+@RestController
 public class HelloController {
 
     private final HelloService helloService;
